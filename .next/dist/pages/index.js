8017a439dcfabfdc750910c08097cdaf8f48e42e
Le Thanh (particle4dev01) <particle4dev01@gmail.com>
2017-10-19 17:16:43 +0700

    navigate between pages

--- .next/dist/pages/index.js
+++ .next/dist/pages/index.js
@@ -8,23 +8,36 @@ var _react = require("react");
 
 var _react2 = _interopRequireDefault(_react);
 
+var _link = require("next/dist/lib/link.js");
+
+var _link2 = _interopRequireDefault(_link);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var _jsxFileName = "/var/www/learnNewJs/pages/index.js?entry";
+
 
 var Index = function Index() {
   return _react2.default.createElement("div", {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 2
+      lineNumber: 4
     }
-  }, _react2.default.createElement("p", {
+  }, _react2.default.createElement(_link2.default, { href: "/about", __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    }
+  }, _react2.default.createElement("a", { style: { fontSize: 20 }, __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    }
+  }, "About Page")), _react2.default.createElement("p", {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 3
+      lineNumber: 8
     }
   }, "Hello Next.js"));
 };
 
 exports.default = Index;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIkluZGV4Il0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7OztBQUFBLElBQU0sUUFBUSxTQUFSLEFBQVEsUUFBQTt5QkFDWixjQUFBOztnQkFBQTtrQkFBQSxBQUNFO0FBREY7QUFBQSxHQUFBLGtCQUNFLGNBQUE7O2dCQUFBO2tCQUFBO0FBQUE7QUFBQSxLQUZVLEFBQ1osQUFDRTtBQUZKLEFBTUE7O2tCQUFBLEFBQWUiLCJmaWxlIjoiaW5kZXguanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL3Zhci93d3cvbGVhcm5OZXdKcyJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIkxpbmsiLCJJbmRleCIsImZvbnRTaXplIl0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7O0FBQUEsQUFBTzs7Ozs7Ozs7O0FBRVAsSUFBTSxRQUFRLFNBQVIsQUFBUSxRQUFBO3lCQUNaLGNBQUE7O2dCQUFBO2tCQUFBLEFBQ0U7QUFERjtBQUFBLEdBQUEsa0JBQ0UsQUFBQyxnQ0FBSyxNQUFOLEFBQVc7Z0JBQVg7a0JBQUEsQUFDRTtBQURGO3FCQUNFLGNBQUEsT0FBRyxPQUFPLEVBQUUsVUFBWixBQUFVLEFBQVk7Z0JBQXRCO2tCQUFBO0FBQUE7S0FGSixBQUNFLEFBQ0UsQUFFRixnQ0FBQSxjQUFBOztnQkFBQTtrQkFBQTtBQUFBO0FBQUEsS0FMVSxBQUNaLEFBSUU7QUFMSixBQVNBOztrQkFBQSxBQUFlIiwiZmlsZSI6ImluZGV4LmpzP2VudHJ5Iiwic291cmNlUm9vdCI6Ii92YXIvd3d3L2xlYXJuTmV3SnMifQ==
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+import Link from 'next/link'
+
 const Index = () => (
   <div>
+    <Link href="/about">
+      <a style={{ fontSize: 20 }}>About Page</a>
+    </Link>
     <p>Hello Next.js</p>
   </div>
 )
